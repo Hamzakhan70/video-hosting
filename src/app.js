87e48f1e5f207a869663743ee3c277a5bdb1be88
unknown <Hamzakhan70>
2025-02-21 12:28:05 +0500

    register controller is completed and check on postman

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-const app = express();
+import userRouter from "./routes/user.routes.js";
 
+const app = express();
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
@@ -13,4 +14,6 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+app.use("/api/v1/users", userRouter);
 export { app };
